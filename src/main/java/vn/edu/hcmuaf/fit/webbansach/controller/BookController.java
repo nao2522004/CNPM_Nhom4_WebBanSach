9ec80d848187b3275c84cd0262c1e29658842b19
nao2522004 <22130163@st.hcmuaf.edu.vn>
2025-05-18 22:53:05 +0700

    update code

--- src/main/java/vn/edu/hcmuaf/fit/webbansach/controller/BookController.java
+++ src/main/java/vn/edu/hcmuaf/fit/webbansach/controller/BookController.java
@@ -4,14 +4,15 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.hcmuaf.fit.webbansach.model.dto.BookDto;
 import vn.edu.hcmuaf.fit.webbansach.model.entity.Books;
 import vn.edu.hcmuaf.fit.webbansach.service.BookService;
 
+import java.util.HashMap;
 import java.util.Map;
 
-// BookController.java
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -19,11 +20,32 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping
-    public ResponseEntity<?> addBook(@RequestBody @Valid BookDto dto) {
-        Books saved = bookService.createBook(dto);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(Map.of("id", saved.getId()));
+    public ResponseEntity<?> addBook(
+            // 13.1.1.4 @RequestBody @Valid BookDto dto,
+            //BindingResult bindingResult
+            @RequestBody @Valid BookDto dto, BindingResult bindingResult // Thêm BindingResult để bắt lỗi
+    ) {
+        // Xử lý validation errors
+        //13.1.2.6 if(bindingResult.hasErrors()){}
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(error -> {
+                errors.put(error.getField(), error.getDefaultMessage());
+            });
+            //13.1.2.7 ResponseEntity.badRequest().body(errors)
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        try {
+            // 2. Tạo sách
+            //13.1.1.6 Gọi hàm createBook(dto)
+            Books saved = bookService.createBook(dto);
+            //13.1.1.11 ReponseEntity.status(HttpStatus.CREATE ).body(Map.of("id", saved.getId()))
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("id", saved.getId()));
+        } catch (IllegalArgumentException ex) {
+            // 3. Bắt trùng lặp
+            //13.1.2.11 ResponseEntity.badRequest().body(Map.of("Error", ex.getMessage()))
+            return ResponseEntity.badRequest().body(Map.of("Error", ex.getMessage()));
+        }
     }
 }
-
